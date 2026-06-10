f78f057ae417525d706ed2794cb32ac9ec098416
DungxCharles <nguyentiendungok@gmail.com>
2026-06-10 12:57:03 +0700

    feat: add an easy canva painter in java

---
+++ src/hust/soict/globalict/javafx/PainterLauncher.java
@@ -0,0 +1,7 @@
+package hust.soict.globalict.javafx;
+
+public class PainterLauncher {
+    public static void main(String[] args) {
+        Painter.main(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module GUIProject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
+
+    opens hust.soict.globalict.javafx to javafx.fxml;
+    exports hust.soict.globalict.javafx;
+    exports hust.soict.globalict.swing;
+}
